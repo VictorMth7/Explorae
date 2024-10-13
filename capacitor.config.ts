f77262170189264a7459d43cdb7913e74a395c19
victormth7 <victor.matheus690@yahoo.com.br>
2024-10-13 16:13:12 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'explorae',
+  webDir: 'www'
+};
+
+export default config;
